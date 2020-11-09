ed1a0c033f35a1a9fdfd69cc7b4d2b77afa5a3d3
Fabio Muller <fabioml10@yahoo.com.br>
2020-11-09 20:42:27 -0300

    Products model configured

---
+++ app/models/product.rb
@@ -0,0 +1,8 @@
+class Product < ApplicationRecord
+  belongs_to :productable, polymorphic: true
+
+  validates :name, presence: true, uniqueness: {case_sensitive: false}
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0}
+  validates :productable, presence: true
+end
