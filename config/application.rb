275cea4971add17584ec05734c1261f133d054db
Fabio Muller <fabioml10@yahoo.com.br>
2020-11-09 19:52:20 -0300

    Rspec installed

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module EcommerceApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.I18n.default_locale = :'pt-BR'
-    config.I18n.load_path += Dir[Rails.root.join('config/locales/**/**.{rb, yml}')]
+    config.i18n.default_locale = :'pt-BR'
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/**.{rb, yml}')]
   end
 end
