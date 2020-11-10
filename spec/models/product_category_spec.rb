c9177f4b2a152fbe339cf7ca884edb3edacf222c
Fabio Muller <fabioml10@yahoo.com.br>
2020-11-09 21:12:58 -0300

    ProductCategory model configured

---
+++ spec/models/product_category_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe ProductCategory, type: :model do
+  # subject { build(:product) }
+
+  # it { is_expected.to validate_presence_of(:name)}
+  # it { is_expected.to validate_uniqueness_of(:name).case_insensitive} #ver model
+
+  # it { is_expected.to validate_presence_of(:description)}
+  # it { is_expected.to validate_presence_of(:price)}
+  # it { is_expected.to validate_numericality_of(:price).is_greater_than(0)}
+  # it { is_expected.to validate_presence_of(:productable)}
+  it { is_expected.to belong_to :product}
+  it { is_expected.to belong_to :category}
+end
