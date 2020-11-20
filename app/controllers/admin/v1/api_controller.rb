ab140e770d3669837d496818505439543eb68395
Fabio Muller <fabioml10@yahoo.com.br>
2020-11-20 12:44:41 -0300

    Create and update category

--- app/controllers/admin/v1/api_controller.rb
+++ app/controllers/admin/v1/api_controller.rb
@@ -1,5 +1,13 @@
 module Admin::V1
   class ApiController < ApplicationController
     include Authenticable
+
+    def render_error(message: nil, fields: nil, status: :unprocessable_entity)
+      errors = {}
+      errors['message'] = message if message.present?
+      errors['fields'] = fields if fields.present?
+      render json: { errors: errors }, status: status
+    end
+    
   end
 end
