e7aa0309823ffe24500c1aad5c8ed9e96dfc859b
Fabio Muller <fabioml10@yahoo.com.br>
2020-12-16 19:01:37 -0300

    Tests done

--- spec/requests/admin/v1/products/client_request_spec.rb
+++ spec/requests/admin/v1/products/client_request_spec.rb
@@ -1,32 +1,49 @@
 require 'rails_helper'
 
-RSpec.describe "Client::V1::Products as :client", type: :request do
+RSpec.describe "Admin V1 Products as :client", type: :request do
   let(:user) { create(:user, profile: :client) }
 
   context "GET /products" do
-    let(:url) { "/admin/v1/products"}
+    let(:url) { "/admin/v1/products" }
     let!(:products) { create_list(:product, 5) }
-    before(:each) {get url, headers: auth_header(user)}
+    
+    before(:each) { get url, headers: auth_header(user) }
+
     include_examples "forbidden_access"
   end
 
   context "POST /products" do
     let(:url) { "/admin/v1/products" }
-    before(:each) {post url, headers: auth_header(user)}
+    
+    before(:each) { post url, headers: auth_header(user) }
+
     include_examples "forbidden_access"
   end
 
-  context "PATCH /products" do
-    let!(:product) { create(:product) }
-    let(:url) { "/admin/v1/products/#{product.id}"}
-    before(:each) {patch url, headers: auth_header(user)}
+  context "GET /products/:id" do
+    let(:product) { create(:product) }
+    let(:url) { "/admin/v1/products/#{product.id}" }
+
+    before(:each) { get url, headers: auth_header(user) }
+
     include_examples "forbidden_access"
   end
 
-  context "DELETE /products" do
+  context "PATCH /products/:id" do
+    let(:product) { create(:product) }
+    let(:url) { "/admin/v1/products/#{product.id}" }
+
+    before(:each) { patch url, headers: auth_header(user) }
+
+    include_examples "forbidden_access"
+  end
+
+  context "DELETE /products/:id" do
     let!(:product) { create(:product) }
     let(:url) { "/admin/v1/products/#{product.id}" }
-    before(:each) {delete url, headers: auth_header(user)}
+
+    before(:each) { delete url, headers: auth_header(user) }
+
     include_examples "forbidden_access"
   end
 end
